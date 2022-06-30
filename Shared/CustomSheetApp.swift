b598d4bd25c57c8a83fb1d6637bc84aee4240f88
Steven Balan <srbalan@outlook.ph>
2022-06-30 14:56:18 +0800

    Initial Commit

---
+++ Shared/CustomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSheetApp.swift
+//  Shared
+//
+//  Created by Steven Rhey Balan on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
